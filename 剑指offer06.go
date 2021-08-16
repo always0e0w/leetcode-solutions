6d3c0c477bf3edd5c20074b28b4a6479f4ee0d7b
processzero <processzero@163.com>
2021-08-16 09:33:07 +0800

    刷题

--- 剑指offer06.go
+++ 剑指offer06.go
@@ -7,14 +7,15 @@ package main
 
 // 从尾到头打印链表
 func reversePrint(head *ListNode) []int {
-	arr := helper(head, []int{})
-	return arr
-}
-
-func helper(head *ListNode, arr []int) []int {
-	if head == nil {
-		return arr
+	arr := make([]int, 0)
+	var f func(*ListNode)
+	f = func(head *ListNode) {
+		if head == nil {
+			return
+		}
+		f(head.Next)
+		arr = append(arr, head.Val)
 	}
-	arr = append(helper(head.Next, arr), head.Val)
+	f(head)
 	return arr
 }
---
+++ 剑指offer24.go
@@ -0,0 +1,12 @@
+package main
+
+// 反转链表
+func reverseList(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	newHead := reverseList(head.Next)
+	head.Next.Next = head
+	head.Next = nil
+	return newHead
+}
